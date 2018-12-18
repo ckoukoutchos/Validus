464dceb809cecb2fa4d7f400ae59a8dcb76b3743
Corbin Koukoutchos <ckoukoutchos@gmail.com>
2018-12-17 19:52:23 -0800

    fixed error with regex validator

--- src/validators/validators.js
+++ src/validators/validators.js
@@ -8,8 +8,8 @@ export default class Validators {
    * @returns boolean
    */
   static email(value) {
-    return value.test(
-      /^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/
+    return /^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/.test(
+      value
     );
   }
 
@@ -56,12 +56,12 @@ export default class Validators {
   }
 
   /**
-   * @description checks if value matches Regex pattern
-   * @param {Regex} pattern Regex
+   * @description checks if value matches RegExp pattern
+   * @param {RegExp} pattern RegExp
    * @returns function
    */
-  static pattern(pattern) {
-    return value => pattern.test(value);
+  static regex(regex) {
+    return value => regex.test(value);
   }
 
   /**
