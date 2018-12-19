6cbe6fe942a7f55955715ff328f2c78ed8fa49fd
Corbin Koukoutchos <ckoukoutchos@gmail.com>
2018-12-18 18:16:26 -0800

    added docs for FormSugar

--- src/form-sugar/FormSugar.js
+++ src/form-sugar/FormSugar.js
@@ -3,23 +3,23 @@ import FormField from '../form-field/FormField';
 import FormGroup from '../form-group/FormGroup';
 
 class FormSugar {
-  static field(values) {
+  static field([fieldName, type, value, validators, onChange]) {
     return (
       <FormField
-        key={values[0]}
-        fieldName={values[0]}
-        type={values[1]}
-        value={values[2]}
-        validators={values[3]}
-        onChange={values[4]}
+        key={fieldName}
+        fieldName={fieldName}
+        type={type}
+        value={value}
+        validators={validators}
+        onChange={onChange}
       />
     );
   }
 
-  static group(values) {
+  static group([groupName, formFields, onChange, value]) {
     return (
-      <FormGroup groupName={values[0]} onChange={values[2]} values={values[3]}>
-        {values[1]}
+      <FormGroup groupName={groupName} onChange={onChange} value={value}>
+        {formFields}
       </FormGroup>
     );
   }
